7c4d3c2bc402163b86d7a209b6a9ac0971bb881e
Quentin Macé <quentin.mace.1110@gmail.com>
2024-01-12 09:27:47 +0100

    Class 'Task' was made and README updated

--- src/toDoApp.java
+++ src/toDoApp.java
@@ -1,5 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 public class toDoApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+	}
+}
+
+class Task {
+	String title;
+	String description;
+	LocalDateTime creation_date;
+	Boolean completed;
+	LocalDateTime completion_date;
+	LocalDate deadline_date;
+	Task(String title, String description, LocalDate deadline_date) {
+		this.title = title;
+		this.description = description;
+		this.creation_date = LocalDateTime.now();
+		this.completed = false;
+		this.completion_date = LocalDateTime.now();
+		this.deadline_date = deadline_date;
 	}
 }
